240ddf5626236df3d2b00d7f5462d183473a01ee
nogovitz <nogovitz00@gmail.com>
2024-06-27 15:12:14 +0200

    added react hook forms to sign in form

--- src/components/custom-input/CustomInput.tsx
+++ src/components/custom-input/CustomInput.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { cloneElement, useState } from 'react';
+import React, { cloneElement, forwardRef, useState } from 'react';
 import { useMountedTheme } from '@/hooks/useMountedTheme';
 import styles from './custom-input.module.scss';
 import { FieldError, FieldErrorsImpl, Merge } from 'react-hook-form';
@@ -19,87 +19,107 @@ interface ICustomInput {
     | undefined;
   actionRoute?: string;
   actionLabel?: string;
+  [key: string]: any;
 }
 
 const iconSize = 30;
 
-export default function CustomInput({
-  name,
-  placeholder,
-  icon,
-  type,
-  isPasswordField = false,
-  error,
-  actionRoute,
-  actionLabel,
-}: ICustomInput) {
-  const { mounted, theme } = useMountedTheme();
-  const [focus, setFocus] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
+const CustomInput = forwardRef<HTMLInputElement, ICustomInput>(
+  (
+    {
+      name,
+      placeholder,
+      icon,
+      type,
+      isPasswordField = false,
+      error,
+      actionRoute,
+      actionLabel,
+      ...props
+    },
+    ref
+  ) => {
+    const { mounted, theme } = useMountedTheme();
+    const [focus, setFocus] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
-  const themeClass = theme ? styles[theme] : '';
+    const themeClass = theme ? styles[theme] : '';
 
-  if (!mounted) {
-    return null;
-  }
+    if (!mounted) {
+      return null;
+    }
 
-  return (
-    <div
-      className={`${styles.customInput} ${themeClass} ${
-        focus ? styles.focused : styles.notFocused
-      }
+    function capitalizeFirstLetter(string: string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    return (
+      <div
+        className={`${styles.customInput} ${themeClass} ${
+          focus ? styles.focused : styles.notFocused
+        }
         ${error ? styles.error : styles.noError}`}
-    >
-      <div className={styles.customInput__labelContainer}>
-        <label
-          className={`${
-            styles.customInput__labelContainer__label
-          }  ${themeClass} ${focus ? styles.focused : styles.notFocused}`}
-        >
-          {name}
-        </label>
-        {actionLabel && actionRoute && (
-          <Link href={actionRoute}>{actionLabel}</Link>
+      >
+        <div className={styles.customInput__labelContainer}>
+          <label
+            htmlFor={name}
+            className={`${
+              styles.customInput__labelContainer__label
+            }  ${themeClass} ${focus ? styles.focused : styles.notFocused}`}
+          >
+            {capitalizeFirstLetter(name)}
+          </label>
+          {actionLabel && actionRoute && (
+            <Link href={actionRoute}>{actionLabel}</Link>
+          )}
+        </div>
+        <div className={styles.customInput__inputContainer}>
+          {icon &&
+            cloneElement(icon, {
+              size: iconSize,
+              className: `${
+                styles.customInput__inputContainer__icon
+              }  ${themeClass} ${focus ? styles.focused : styles.notFocused}`,
+            })}
+          <input
+            className={styles.customInput__inputContainer__input}
+            name={name}
+            placeholder={placeholder}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            type={showPassword ? 'text' : type}
+            ref={ref}
+            {...props}
+          />
+          {isPasswordField &&
+            (showPassword ? (
+              <IoEyeOutline
+                size={iconSize}
+                className={`${
+                  styles.customInput__inputContainer__icon
+                } ${themeClass} ${focus ? styles.focused : styles.notFocused}`}
+                onClick={() => setShowPassword(false)}
+              />
+            ) : (
+              <IoEyeOffOutline
+                size={iconSize}
+                className={`${
+                  styles.customInput__inputContainer__icon
+                } ${themeClass} ${focus ? styles.focused : styles.notFocused}`}
+                onClick={() => setShowPassword(true)}
+              />
+            ))}
+        </div>
+        {error && error.message && (
+          <p className={styles.customInput__error}>
+            {error.message.toString()}
+          </p>
         )}
       </div>
-      <div className={styles.customInput__inputContainer}>
-        {icon &&
-          cloneElement(icon, {
-            size: iconSize,
-            className: `${
-              styles.customInput__inputContainer__icon
-            }  ${themeClass} ${focus ? styles.focused : styles.notFocused}`,
-          })}
-        <input
-          className={styles.customInput__inputContainer__input}
-          name={name}
-          placeholder={placeholder}
-          onFocus={() => setFocus(true)}
-          onBlur={() => setFocus(false)}
-          type={showPassword ? 'text' : type}
-        />
-        {isPasswordField &&
-          (showPassword ? (
-            <IoEyeOutline
-              size={iconSize}
-              className={`${
-                styles.customInput__inputContainer__icon
-              } ${themeClass} ${focus ? styles.focused : styles.notFocused}`}
-              onClick={() => setShowPassword(false)}
-            />
-          ) : (
-            <IoEyeOffOutline
-              size={iconSize}
-              className={`${
-                styles.customInput__inputContainer__icon
-              } ${themeClass} ${focus ? styles.focused : styles.notFocused}`}
-              onClick={() => setShowPassword(true)}
-            />
-          ))}
-      </div>
-      {error && error.message && (
-        <p className={styles.customInput__error}>{error.message.toString()}</p>
-      )}
-    </div>
-  );
-}
+    );
+  }
+);
+
+CustomInput.displayName = 'CustomInput';
+
+export default CustomInput;
--- src/components/sign-in-form/SignInForm.tsx
+++ src/components/sign-in-form/SignInForm.tsx
@@ -8,35 +8,77 @@ import CustomButton from '../custom-button/CustomButton';
 
 import styles from './sign-in-form.module.scss';
 import { useMountedTheme } from '@/hooks/useMountedTheme';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { signInSchema } from './validationSchema';
+
+interface Inputs {
+  email: string;
+  password: string;
+}
 
 export default function SignInForm() {
   const { mounted, theme } = useMountedTheme();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: yupResolver(signInSchema),
+  });
+
   const themeClass = theme ? styles[theme] : '';
 
   if (!mounted) {
     return null;
   }
 
+  const onSubmit = async (data: Inputs) => {
+    console.log('Form submitted with data:', data);
+    try {
+      console.log('LOGIN DATA: ', data);
+      // Perform the login logic here
+      // If you have a mock API call, you can add it here
+    } catch (error: any) {
+      console.log('Error occurred:', error);
+      if (error.message === 'Incorrect email or password') {
+        setError('email', { type: 'manual', message: error.message });
+        setError('password', { type: 'manual', message: error.message });
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
   return (
-    <form className={`${styles.form} ${themeClass}`}>
+    <form
+      className={`${styles.form} ${themeClass}`}
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className={styles.form__textFields}>
         <CustomInput
-          name="Email"
           placeholder="johndoe@todou.com"
           icon={<FiMail />}
           type="email"
+          id="email"
+          error={errors.email}
+          {...register('email', { required: 'This field is required' })}
         />
+
         <CustomInput
-          name="Password"
           placeholder="********"
           icon={<IoKeyOutline />}
           type="password"
           isPasswordField
           actionLabel="Forgot Password?"
           actionRoute="/forgot-password"
+          id="password"
+          error={errors.password}
+          {...register('password', { required: 'This field is required' })}
         />
       </div>
-      <CustomButton>Sign In</CustomButton>
+      <CustomButton type="submit">Sign In</CustomButton>
       <p className={styles.form__createAccount}>
         Don’t have an account yet? <span>Sign up</span> here!
       </p>
