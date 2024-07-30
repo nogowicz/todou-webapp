a826afd4efdd6657aa70e4a8438cd10a414b1a02
nogovitz <nogovitz00@gmail.com>
2024-07-30 11:36:04 +0200

    commit to save state before adding Parallel Routes

--- src/app/[locale]/(authenticated)/lists/[slug]/page.tsx
+++ src/app/[locale]/(authenticated)/lists/[slug]/page.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 
 import styles from './page.module.scss';
 import { getLists } from '@/actions/List';
 import { IList } from '@/types/List';
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
+import {
+  listColorTheme,
+  listIconTheme,
+} from '@/components/list-item/ListStyles';
+import { BsThreeDots } from 'react-icons/bs';
+
+const ICON_SIZE = 50;
 
 export default async function Page({ params }: { params: Params }) {
   const data = await getLists();
 
-  const currentList = data.find((list: IList) => list.listId === +params.slug);
+  const list: IList = data.find((list: IList) => list.listId === +params.slug);
 
-  if (!currentList) {
+  if (!list) {
     return <div>List not found</div>;
   }
 
-  return <div className={styles.listName}>{currentList.listName}</div>;
+  return (
+    <div className={styles.listPage}>
+      <div className={styles.listPage__upperContainer}>
+        <div className={styles.listPage__upperContainer__leftSide}>
+          {cloneElement(listIconTheme[list.iconId], {
+            color: listColorTheme[list.colorVariant],
+            size: ICON_SIZE,
+          })}
+          <h3>{list.listName}</h3>
+        </div>
+        <BsThreeDots size={ICON_SIZE} />
+      </div>
+    </div>
+  );
 }
