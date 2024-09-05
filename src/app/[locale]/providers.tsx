718dfe361de6d2596c086e7cb2ff6630374ebefa
nogovitz <nogovitz00@gmail.com>
2024-09-05 12:57:46 +0200

    fixed redirection error

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -6,10 +6,12 @@ import UserProvider from '@/utils/Providers/UserProvider';
 export async function Providers({ children }: { children: React.ReactNode }) {
   const messages = await getMessages();
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <NextIntlClientProvider messages={messages}>
-        <UserProvider>{children}</UserProvider>
-      </NextIntlClientProvider>
-    </ThemeProvider>
+    <UserProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </ThemeProvider>
+    </UserProvider>
   );
 }
